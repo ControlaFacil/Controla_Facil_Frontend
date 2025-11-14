b8ee87318e7bccde9cfed1e651997ad86fe4227d
Guilherme Galdino <guigaldino.sp@gmail.com>
2025-11-13 21:40:49 -0300

    Paginas para resultado da integracao

---
+++ src/components/IntegracaoSucesso.jsx
@@ -0,0 +1,13 @@
+// SuccessPage.jsx
+import React from 'react';
+import './style/SuccessPage.css';
+
+export default function SuccessPage({ onGoHome }) {
+  return (
+    <div className="success-container">
+      <h1>Integração realizada com sucesso!</h1>
+      <p>Agora você pode acessar o sistema normalmente.</p>
+      <button onClick={onGoHome}>Ir para Home</button>
+    </div>
+  );
+}
