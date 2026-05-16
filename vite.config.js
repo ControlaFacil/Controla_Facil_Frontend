f5c64db46444c2c875ced8420aaa9f11bc15211e
Guilherme Galdino <guigaldino.sp@gmail.com>
2026-05-15 22:50:14 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': {
-        target: 'https://controlafacilbackend-production.up.railway.app', 
+        target: 'https://serrated-deodorize-ruckus.ngrok-free.dev', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api') 
       }
